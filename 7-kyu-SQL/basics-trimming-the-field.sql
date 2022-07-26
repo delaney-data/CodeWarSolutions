99707c6183fd7e68df3cc5a2e193b0523225fb2c
Delaney Underwood <66498659+delaney-data@users.noreply.github.com>
2022-07-26 11:49:56 -0500

    Update basics-trimming-the-field.sql
    7-kyu solution

--- 7-kyu-SQL/basics-trimming-the-field.sql
+++ 7-kyu-SQL/basics-trimming-the-field.sql
@@ -2,14 +2,13 @@
 /*Challenge: You have access to a table of monsters as follows:
 
 monsters schema
-    id
+   id
   name
   legs
   arms
   characteristics
 
 The monsters in the provided table have too many characteristics, they really only need one each. Your job is to trim the characteristics down so that each monster only has one. If there is only one already, provide that. If there are multiple, provide only the first one (don't leave any commas in there).
-
 You must return a table with the format as follows:
 
 output schema 
@@ -17,12 +16,13 @@ output schema
   name
   characteristic
   Order by id
-*/
-  
+*/  
+
 -- Solution:
 
 SELECT id,
        name, 
        split_part(characteristics, ',', 1) as characteristic
-  FROM monsters
-ORDER BY id
+
+FROM monsters
+ORDER BY id;
