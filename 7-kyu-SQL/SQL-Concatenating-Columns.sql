e4a318d3c32375085d805e0db7663954d0a55c4d
Delaney Underwood <66498659+delaney-data@users.noreply.github.com>
2022-08-01 11:44:04 -0500

    Create SQL-Concatenating-Columns.sql

---
+++ 7-kyu-SQL/SQL-Concatenating-Columns.sql
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/59440034e94fae05b2000073
+
+Given the table below:
+** names table schema **
+    id
+    prefix
+    first
+    last
+    suffix
+    
+Your task is to use a select statement to return a single column table containing 
+the full title of the person (concatenate all columns together except id), as follows:
+
+** output table schema **
+    title
+
+Don't forget to add spaces.
+*/
+
+SELECT CONCAT_WS
+(' ', prefix, first, last, suffix) AS title
+FROM names;
